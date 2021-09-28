e5868b29c48a2f38648af836111db1f84235824d
ahkamul <sasraishougat@gmail.com>
2021-09-28 10:28:33 +0600

    2 files index6.html and index6.js initiated to check number method (#6)

---
+++ index6.js
@@ -0,0 +1,40 @@
+// Ex. 1 
+
+// var num = 20
+// num = toString (num)
+
+// console.log (typeof (num))
+
+// Ex. 2 
+
+// var num = "20";
+
+// //num = parseInt (num);
+// num = parseFloat (num);
+
+// document.write (typeof (num));
+
+// Ex. 3
+
+//var number = 2.5678;
+
+//document.write (number.toFixed (1)) 
+// document.write (number.toFixed (2))
+// document.write (number.toFixed (3))
+// document.write (number.toFixed (4))
+// document.write (number.toFixed (5))
+// document.write (number.toFixed (0))
+
+
+// document.write (number.toPrecision (1)) 
+// document.write (number.toPrecision (2))
+// document.write (number.toPrecision (3))
+// document.write (number.toPrecision (4))
+// document.write (number.toPrecision (5))
+// document.write (number.toPrecision (0))  // Nothing will be printed
+
+
+// Ex. 3
+
+//document.write (Number ("12"))
+document.write (typeof (Number ("12")))
