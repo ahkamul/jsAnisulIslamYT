38e32146cb542b618f8d423fc67d3d8fbaf5eb91
ahkamul <sasraishougat@gmail.com>
2021-10-02 12:50:09 +0600

    Codes digit programme added using switch in index15.js

--- index15.js
+++ index15.js
@@ -1,35 +1,85 @@
 // Making Digit Programme with if, else if & else: 
 
-var digit = prompt ("Enter a digit :")
+// var digit = prompt ("Enter a digit :")
 
-if (digit == 0)
-    document.write ("Zero");
+// if (digit == 0)
+//     document.write ("Zero");
 
-else if (digit == 1)
-    document.write ("One"); 
+// else if (digit == 1)
+//     document.write ("One"); 
 
-else if (digit == 2)
-    document.write ("Two"); 
+// else if (digit == 2)
+//     document.write ("Two"); 
 
-else if (digit == 3)
-    document.write ("Three"); 
+// else if (digit == 3)
+//     document.write ("Three"); 
 
-else if (digit == 4)
-    document.write ("Four"); 
+// else if (digit == 4)
+//     document.write ("Four"); 
 
-else if (digit == 5)
-    document.write ("Five"); 
+// else if (digit == 5)
+//     document.write ("Five"); 
 
-else if (digit == 6)
-    document.write ("Six"); 
+// else if (digit == 6)
+//     document.write ("Six"); 
 
-else if (digit == 7)
-    document.write ("Seven"); 
+// else if (digit == 7)
+//     document.write ("Seven"); 
 
-else if (digit == 8)
-    document.write ("Eight"); 
+// else if (digit == 8)
+//     document.write ("Eight"); 
 
-else if (digit == 9)
-    document.write ("Nine"); 
-else 
-    document.write ("Not a digit"); 
+// else if (digit == 9)
+//     document.write ("Nine"); 
+// else 
+//     document.write ("Not a digit"); 
+
+
+// Making Digit Programme using switch :
+
+var digit = prompt ("Enter any digit :");
+
+switch (digit) {
+    case "0":
+        document.write ("Zero");
+        break;
+
+    case "1":
+        document.write ("One");
+        break;
+
+    case "2":
+        document.write ("Two");
+        break;
+
+    case "3":
+        document.write ("Three");
+        break;
+
+    case "4":
+        document.write ("Four");
+        break;
+
+    case "5":
+        document.write ("Five");
+        break;
+
+    case "6":
+        document.write ("Six");
+        break;
+
+    case "7":
+        document.write ("Seven");
+        break;
+
+    case "8":
+        document.write ("Eight");
+        break;
+
+    case "9":
+        document.write ("Nine");
+        break;
+
+    default:
+        document.write ("Not a digit");
+}
