fcf74a78c367ac0e7c78d53a191e6b9ec8b85cd1
ahkamul <sasraishougat@gmail.com>
2021-10-02 13:06:36 +0600

    Codes added of Vowel & Consonant Programme using switch

--- index15.js
+++ index15.js
@@ -37,49 +37,81 @@
 
 // Making Digit Programme using switch :
 
-var digit = prompt ("Enter any digit :");
+// var digit = prompt ("Enter any digit :");
 
-switch (digit) {
-    case "0":
-        document.write ("Zero");
+// switch (digit) {
+//     case "0":
+//         document.write ("Zero");
+//         break;
+
+//     case "1":
+//         document.write ("One");
+//         break;
+
+//     case "2":
+//         document.write ("Two");
+//         break;
+
+//     case "3":
+//         document.write ("Three");
+//         break;
+
+//     case "4":
+//         document.write ("Four");
+//         break;
+
+//     case "5":
+//         document.write ("Five");
+//         break;
+
+//     case "6":
+//         document.write ("Six");
+//         break;
+
+//     case "7":
+//         document.write ("Seven");
+//         break;
+
+//     case "8":
+//         document.write ("Eight");
+//         break;
+
+//     case "9":
+//         document.write ("Nine");
+//         break;
+
+//     default:
+//         document.write ("Not a digit");
+// }
+
+
+// Vowel & Consonant Programme using switch :
+
+var letter = prompt ("Enter a letter :");
+
+letter = letter.toLowerCase();
+
+switch (letter) {
+    case "a":
+        document.write ("Vowel");
         break;
 
-    case "1":
-        document.write ("One");
+    case "e":
+        document.write ("Vowel");
         break;
 
-    case "2":
-        document.write ("Two");
+    case "i":
+        document.write ("Vowel");
         break;
 
-    case "3":
-        document.write ("Three");
+    case "o":
+        document.write ("Vowel");
         break;
 
-    case "4":
-        document.write ("Four");
-        break;
-
-    case "5":
-        document.write ("Five");
-        break;
-
-    case "6":
-        document.write ("Six");
-        break;
-
-    case "7":
-        document.write ("Seven");
-        break;
-
-    case "8":
-        document.write ("Eight");
-        break;
-
-    case "9":
-        document.write ("Nine");
+    case "u":
+        document.write ("Vowel");
         break;
 
     default:
-        document.write ("Not a digit");
+        document.write ("Consonant");
 }
